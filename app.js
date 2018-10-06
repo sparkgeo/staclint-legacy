4d391fe45ade3e3a4bc5ebc51cda245e217c3cbe
Dustin Sampson <gridcell@gmail.com>
2018-10-06 14:36:26 -0400

    wip

--- app.js
+++ app.js
@@ -2,3 +2,25 @@ require('./app.css');
 var $ = require('jquery');
 var popper = require('popper.js');
 var bootstrap = require('bootstrap');
+var CodeMirror = require('codemirror');
+
+var jsonEditor = CodeMirror(document.getElementById('editor'), {
+  value: "{\"test\": 343}",
+  mode: "json",
+  lineNumbers: true,
+  theme: 'darcula'
+});
+
+
+var validate = function () {
+  $('#validateForm').addClass('validated');
+};
+
+var addErrors = function(errors) {
+
+};
+
+
+
+// Events
+$('#validateButton').click(validate);
